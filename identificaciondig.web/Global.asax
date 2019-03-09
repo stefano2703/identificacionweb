4e81406ff421b54a174d3495907f5c4728c9afd7
stefano2703 <stefanogtf@gmail.com>
2019-03-09 15:14:09 -0500

    template
    initial

---
+++ identificaciondig.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="identificaciondig.web.Global" Language="C#" %>
---
+++ identificaciondig.web/identificacion/capturaimagen.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace identificaciondig.web.identificacion
+{
+    public partial class capturaimagen : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
